aeeb6e3279a684650e0bbe4aa752c64309de3b97
farrrahhh <18222096@std.stei.itb.ac.id>
2025-01-10 13:32:14 +0700

    Update view path in index method of Telemed_MedicineController

--- app/Controllers/Telemed_MedicineRegisterController.php
+++ app/Controllers/Telemed_MedicineRegisterController.php
@@ -11,7 +11,7 @@ class Telemed_MedicineController extends Controller
     // index
     public function index()
     {
-        return view('patient/Telemed_MedicineRegister');
+        return view('/patient/Telemed_UserMedicineRegister');
     }
 
     public function __construct()
